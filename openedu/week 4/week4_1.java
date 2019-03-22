d79c9ebeb0d55803db4fd46e08d253ef95eb40f9
Vladislav Saraev <work.saraev@gmail.com>
2019-03-22 21:46:15 +0300

    4_1 26 stuck

--- openedu/week 4/week4_1.java
+++ openedu/week 4/week4_1.java
@@ -2,14 +2,14 @@ import java.io.*;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
-import java.util.ArrayList;
+import java.util.Stack;
 
 public class Main {
 
     public static void main(String[] args) throws IOException {
-        StringBuilder data = new StringBuilder();
-        File inputFilePath = new File("input.txt");
-        byte[] bytes = Files.readAllBytes(inputFilePath.toPath());
+        final StringBuilder data = new StringBuilder();
+        final File inputFilePath = new File("input.txt");
+        final byte[] bytes = Files.readAllBytes(inputFilePath.toPath());
         char singleChar;
         for(byte b : bytes) {
             singleChar = (char) b;
@@ -17,23 +17,22 @@ public class Main {
         }
         StringBuilder deleted = new StringBuilder();
         String value;
-        String[] content = data.toString().split("\\n");
         int counter = 0;
+        String[] content = data.toString().split("\\n");
         int actionsValue = Integer.parseInt(content[0].split(" ")[0].trim());
-        ArrayList<Integer>  stack = new ArrayList<>();
+        String[] stack = new String[actionsValue];
         for(int i = 1; i < actionsValue+1; i++){
             if(content[i].split(" ")[0].trim().equals("-")){
                 counter--;
-                deleted.append(stack.get(counter)).append("\n");
-                stack.remove(counter);
+                deleted.append(stack[counter]).append("\n");
             }
             else  {
                 value = content[i].split(" ")[1].trim();
-                stack.add(counter, Integer.valueOf(value));
+                stack[counter] = value;
                 counter++;
             }
         }
-        Path outputFilePath = Paths.get("output.txt");
+        final Path outputFilePath = Paths.get("output.txt");
         Files.write(outputFilePath, deleted.toString().getBytes());
     }
 }
