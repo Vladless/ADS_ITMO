991bf0c9a4ca6ebb485eb0bddd81a65360a54fa4
Vladislav Saraev <work.saraev@gmail.com>
2019-03-23 23:43:50 +0300

    add complete 4_1 and result

--- openedu/week 4/week 4_1.java
+++ openedu/week 4/week 4_1.java
@@ -5,34 +5,23 @@ import java.nio.file.Paths;
 
 public class Main {
 
-    //
     public static void main(String[] args) throws IOException {
-        final StringBuilder data = new StringBuilder();
-        final File inputFilePath = new File("input.txt");
-        final byte[] bytes = Files.readAllBytes(inputFilePath.toPath());
-        char singleChar;
-        for(byte b : bytes) {
-            singleChar = (char) b;
-            data.append(singleChar);
-        }
-        StringBuilder deleted = new StringBuilder();
-        String value;
+        String content;
         int counter = 0;
-        String[] content = data.toString().split("\\n");
-        int actionsValue = Integer.parseInt(content[0].split(" ")[0].trim());
-        String[] stack = new String[actionsValue];
-        for(int i = 1; i < actionsValue+1; i++){
-            if(content[i].split(" ")[0].trim().equals("-")){
+        StringBuilder deleted = new StringBuilder();
+        BufferedReader br = new BufferedReader(new FileReader("input.txt"));
+        int[] stack = new int[Integer.valueOf(br.readLine())];
+        while ((content = br.readLine()) != null) {
+            if(content.split(" ")[0].equals("-")){
                 counter--;
                 deleted.append(stack[counter]).append("\n");
             }
-            else  {
-                value = content[i].split(" ")[1].trim();
-                stack[counter] = value;
+            if(content.split(" ")[0].equals("+")){
+                stack[counter] =  Integer.valueOf(content.split(" ")[1].trim());
                 counter++;
             }
         }
-        final Path outputFilePath = Paths.get("output.txt");
+        Path outputFilePath = Paths.get("output.txt");
         Files.write(outputFilePath, deleted.toString().getBytes());
     }
 }
