049e07e2e388f4c3469326979f92a244f71a6b76
Vladislav Saraev <work.saraev@gmail.com>
2019-03-24 20:07:38 +0300

    add complete 4_2 and result

--- openedu/week 4/week4_2.java
+++ openedu/week 4/week4_2.java
@@ -3,26 +3,28 @@ import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
 import java.util.Collections;
-import java.util.LinkedList;
 
 public class Main {
 
     public static void main(String[] args) throws IOException {
         String content;
-        int fl = 0;
+        int tail = 0;
+        int head = 0;
         StringBuilder deleted = new StringBuilder();
         BufferedReader br = new BufferedReader(new FileReader("input.txt"));
-        LinkedList<Integer> queue = new LinkedList<>();
+        int len = Integer.valueOf(br.readLine());
+        int[] queue = new int[len];
         while ((content = br.readLine()) != null) {
             if(content.split(" ")[0].equals("+")){
-                queue.addFirst(Integer.valueOf(content.split(" ")[1].trim()));
+                if (++tail == len)
+                    tail = 0;
+                queue[tail] = Integer.valueOf(content.split(" ")[1]);
             }
             else{
-                if(fl == 0) {
-                    fl++;
-                    continue;
+                if (++head == len) {
+                    head = 0;
                 }
-                deleted.append(queue.removeLast()).append("\n");
+                deleted.append(queue[head]).append("\n");
             }
         }
         Path outputFilePath = Paths.get("output.txt");
